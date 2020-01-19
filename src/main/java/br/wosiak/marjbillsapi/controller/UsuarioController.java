e7d72f98217085b49d336296a67cc6cc28757c5f
Bruno Wosiak <brunofwosiak@gmail.com>
2020-01-19 12:07:06 -0300

    Override save usuário e Testes
    Override o método save do usuário para retornar a location da criação e implementado test do save no postman com export do environment.

--- src/main/java/br/wosiak/marjbillsapi/controller/UsuarioController.java
+++ src/main/java/br/wosiak/marjbillsapi/controller/UsuarioController.java
@@ -1,8 +1,14 @@
 package br.wosiak.marjbillsapi.controller;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.wosiak.marjbillsapi.dto.UsuarioDTO;
 import br.wosiak.marjbillsapi.service.BasicService;
@@ -18,6 +24,18 @@ public class UsuarioController extends BasicRestController<UsuarioDTO, Long>{
 	@Override
 	protected BasicService<UsuarioDTO, Long> getBasicService() {
 		return this.usuarioService;
+	}
+
+	@Override
+	public ResponseEntity<UsuarioDTO> salvar(@RequestBody UsuarioDTO dto) {
+		try {
+			UsuarioDTO dtoResponse = usuarioService.salvar(dto);
+			URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/"+dtoResponse.getId()).buildAndExpand(dto.getId())
+					.toUri();
+			return ResponseEntity.created(location).body(dtoResponse);
+		}catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 	
 }
